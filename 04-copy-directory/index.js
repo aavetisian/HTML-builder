5fff11583cdf4220958c6a7742b6ed59503f9b9e
Ani <aavetisian@noorlogic.com>
2023-05-08 11:24:29 +0400

    feat: file copied

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,31 @@
+const fs = require('fs');
+const fsPromises = fs.promises;
+const copyFile = fsPromises.copyFile;
+const path = require('path');
+
+async function copyDir() {
+    try {
+        await fsPromises.rm(
+            path.join(__dirname, 'files-copy'),
+            { recursive: true, force: true }
+        );
+
+        await fsPromises.mkdir(
+            path.join(__dirname, 'files-copy'),
+            { recursive: true }
+        );
+
+        console.log('Folder successfully created.');
+
+        const files = await fsPromises.readdir(path.join(__dirname, 'files'));
+        for (const file of files) {
+            const filePath = path.join(__dirname, 'files', file);
+            await copyFile(filePath, path.join(__dirname, 'files-copy', file));
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+copyDir();
 
