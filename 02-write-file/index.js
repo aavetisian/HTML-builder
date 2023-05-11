1647f7c08528bf06aaa1f373458bbf481b6213a0
Ani <aavetisian@noorlogic.com>
2023-05-11 11:23:40 +0400

    refactor: second task implemented

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -3,13 +3,19 @@ const path = require("path");
 
 const { stdout, stdin, exit } = process;
 stdout.write("Hey there! Please enter some text\n");
-stdin.on("data", data =>{
+stdin.on("data", data => {
     const filePath = path.join(__dirname, "file.txt");
-    const writeStream = fs.createWriteStream(filePath);
-    if(data.toString().trim() === "exit"){
+    const writeStream = fs.createWriteStream(filePath, { flags: "a" });
+
+    if (data.toString().trim() === "exit") {
         stdout.write("Thank you, BYE!");
         exit();
     }
+
     writeStream.write(data);
-    
+});
+
+process.on("SIGINT", () => {
+    stdout.write("Thank you, BYE!\n");
+    exit();
 });
